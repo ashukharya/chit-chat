d2db50d6b8124e6f4c46a2a669ebd4fbf703ed4d
ashukharya <ashukharya139@gmail.com>
2023-05-20 13:52:05 +0530

    server commt

--- server/index.js
+++ server/index.js
@@ -6,7 +6,13 @@ const { Server } = require("socket.io");
 require('dotenv').config()
 //const skt = require("socket.io");
 app.use(cors()); 
- 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  next();
+});
 const server = http.createServer(app);
 // const io=skt(server);
 
