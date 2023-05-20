85dc3ae34ebd5185646d5b8f52c2936cc6407f0d
ashukharya <ashukharya139@gmail.com>
2023-05-20 14:39:35 +0530

    server cot

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 //const skt = require("socket.io");
 app.use(cors()); 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://comforting-sunshine-191ae3.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
