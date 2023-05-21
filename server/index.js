9ba0af7b2875ea590506e2777d733cf7834473c8
ashukharya <ashukharya139@gmail.com>
2023-05-21 13:50:27 +0530

    bcki2

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 //const skt = require("socket.io");
 app.use(cors()); 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "https://bleh-blah.netlify.app/");
+  res.setHeader('Access-Control-Allow-Origin', "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
